1cc524559bf88854506b472a38bdc330c515934b
Insanefoam <spartaq123456789@gmail.com>
2020-07-02 12:43:14 +0600

    add column start comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Column from './Column';
 
-const App = () => {
-  return <div>Hello</div>
-}
+const App = () =>
+  <div className='container' style={{fontFamily: 'Montserrat'}}>
+    <div className='Title' style={{textAlign: 'center', marginBottom: '30px'}}>Trello</div>
+    <div className='column-wrapper' style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Column name='TODO' />
+      <Column name='In Progress' />
+      <Column name='Testing' />
+      <Column name='Done' />
+    </div>
+  </div>
 
 export default App;
---
+++ src/Column.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Column = (props) =>
+    <div className='column'>
+        <div className='column__title'>{props.name}</div>
+    </div>
+
+export default Column;
