8b0b0df6e22d92e88a4168062a2c4b3a5bda0e81
Insanefoam <spartaq123456789@gmail.com>
2020-07-04 19:12:39 +0600

    add card popup structure

--- src/CardPopup.js
+++ src/CardPopup.js
@@ -10,7 +10,17 @@ const style = {
 
 const CardPopup = () =>
     <div style={style}>
-        card popup
+        <button>Close</button>
+        <input value='name'></input>
+        <div>Column name</div>
+        <div>Author</div>
+        <button>Delete</button>
+        <input value='Description'></input>
+        <div>
+            <input value='Comment 1'></input>
+            <input value='Comment 2'></input>
+            <input value='Comment 3'></input>
+        </div>
     </div>
 
 export default CardPopup;
